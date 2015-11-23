a7878237324e8d482ca1cb5bb798ed2f439b758c
hinni10 <hinniawesome@hotmail.com>
2015-11-23 16:36:36 +0000

    Bæting

--- hssite/hssite.js
+++ hssite/hssite.js
@@ -1,23 +1,18 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
 }
+
+
+Router.route('/', function () {
+  this.layout('home');
+});
+
+Router.route('/Cards', function () {
+  this.layout('cards');
+});
+
+Router.configure({
+  layoutTemplate: 'main'
+});
